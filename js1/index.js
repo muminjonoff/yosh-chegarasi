a716a193548a1358db1ff16dbb6dd590f3a40fcf
muminjonoff <166128251+muminjonoff@users.noreply.github.com>
2024-06-03 08:24:22 +0500

    Add files via upload

---
+++ js1/index.js
@@ -0,0 +1,130 @@
+
+var forDays = +prompt ("Yoshingizni kirgizing!")
+
+
+if(forDays < 20){
+    document.write("Siz o'smir yoshdasiz")
+
+}
+ else if(forDays <= 20 && forDays <= 40){
+    document.write("siz voyaga yetgan mustaqil yoshdasiz")
+ }
+else if ( forDays >= 65 &&  forDays <= 85){
+    document.write("siz qarimoqdasiz")
+
+}
+else if ( forDays > 85 && forDays <= 100){
+    document.write("siz qaridingiz!")
+}
+// 
+
+// var a = prompt("son kiriting!")
+// if(a =)
+
+
+
+
+
+// var forDays = prompt("Hafta kunini kirgizing")
+
+// switch(forDays){
+//     case "dush":
+//     case "sesh":
+//     case "chor":
+//     case "pay":
+//     case "juma":
+//     document.write("Siz ish kunidasiz ishingizga masuliyatli bo'ling!");
+//     break
+//     break
+//     default:
+//     document.write("Siz boshqa sayyoraga tushdingiz!")
+
+// }
+
+// var a = prompt("Moshina turini kiriting")
+
+
+
+// switch(a1){
+// case "gentra":
+// case "damas":
+// case "nexia":
+// case "matiz":
+// case "jiguli":
+// document.write("bu Uzbekiston mashinasi");
+// break
+// case "audi":
+// case "bmw":
+// case "mercades":
+// case "ferarri":
+// document.write("bu Yevropa mashinasi")
+// break
+// default:
+// document.write("bunday mashinani bilmaymiz! ")
+// }
+
+
+
+
+
+
+// document.write(demo)
+// var a = prompt("yozing!")
+// if ()
+    //     document.write("Dam olish kunlaringizni maroqli o'tkazing");
+//     case "shan":
+//     case "yak":
+
+
+// var students = [
+//     "Otabek",
+//     "Husan",
+//     "Anvar",
+//     "Kamron",
+//     "Xussnidin",
+//     "Shabbona",
+//     "Gulsevar",
+//     "Ziyoda"
+// ]
+// document.write((Math.floor( Math.random("students")*8 )+1 ));
+
+
+
+
+
+// var num1 = +prompt("Birinchi sonni kiriting!");
+// var math = prompt("Amalni  kiriting!");
+// var num2 = +prompt("Ikkinchi sonni kiriting!");
+
+// var a = eval(num1 + math +num2)
+
+// console.log(eval);
+
+
+
+// document.write(a
+
+
+// var a1 = "selection";
+
+
+
+
+// console.log(a1.replace("selection", "info")); joy almashtiradi 
+
+// console.log(a1.split("selection")); array qiladi
+
+
+
+
+// console.log(a1.includes("selection"));  ichidagini  tekshiradi
+
+
+// var city = 'Toshkent';
+// console.log(city.substring(0, 8));
+
+
+
+
+
+
